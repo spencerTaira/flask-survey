01410c971641129f804187113f3e0fed97485e2f
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-13 17:39:34 -0700

    Finished main

--- app.py
+++ app.py
@@ -7,3 +7,55 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get("/")
+def show_home_page():
+    # """Return initial page with title, instructions, and button"""
+    # responses[:] = []
+    title = survey.title
+    instruction = survey.instructions
+    return render_template(
+        "survey_start.html",
+        title=title,
+        instruction=instruction
+    )
+
+@app.post("/begin")
+def begin_survey():
+    responses.clear()
+    return redirect("/questions/0")
+
+
+@app.get("/questions/<int:nums>")
+def show_question(nums):
+    """Show question"""
+    question = survey.questions[nums]
+    return render_template(
+        "question.html",
+        question=question,
+        question_number=nums
+    )
+
+
+@app.post("/answer")
+def handle_answer():
+    """Save answer to global variable and redirect to next question"""
+    answer = request.form["answer"]
+    question_number = int(request.form["current_question"]) + 1
+    responses.append(answer)
+    print(len(responses), responses)
+    
+    if len(responses) == len(survey.questions):
+        return redirect("/thankyou")
+    else:
+        return redirect(f"/questions/{question_number}")
+
+
+@app.get("/thankyou")
+def handle_completion():
+    """Redirect user to completion page once done with survey"""
+    questions_and_answers = zip(survey.questions, responses)
+    return render_template("completion.html", questions_and_answers=questions_and_answers)
